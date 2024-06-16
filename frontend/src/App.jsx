55aff6269fe9fce25fadcaab9f4e958b907b5008
Sudhais <87361938+sudhais@users.noreply.github.com>
2024-06-16 16:39:00 +0530

    header modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Home from './pages/HomePage'
 import Footer from './components/Footer'
+import LoginPage from './pages/LoginPage'
 
 
 
@@ -14,7 +15,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Routes>
-
+            <Route path="/login" Component={LoginPage}/>
             <Route path="/" Component={Home}/>
           </Routes>
         </Container>
